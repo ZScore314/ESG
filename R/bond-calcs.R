ae0ea933da0bb577f5a472b3f8fb72a56ecf51b5
Zach Eisenstein <z.eisenstein2@gmail.com>
2022-02-09 22:02:08 -0500

    add bond calcs for short rate models

--- R/bond-calcs.R
+++ R/bond-calcs.R
@@ -10,7 +10,7 @@
 #' @export
 #'
 #' @examples
-Vasicek1fPrice <- function(r, a, b, s, t){
+Vasicek1fPrice <- function(r, a, b, v, t){
   # returns the zero bond price that pays $1 at time t under a 1-factor Vasicek model
   # see Hull p.685
 
@@ -18,8 +18,8 @@ Vasicek1fPrice <- function(r, a, b, s, t){
   B <-  (1 - exp(-a * t)) / a
 
   # A Term
-  A1 <- ((B - t) * (a * a * b - s * s / 2)) / (a^2)
-  A2 <- (s * s * (B^2) / (4 * a))
+  A1 <- ((B - t) * (a * a * b - v^2 / 2)) / (a^2)
+  A2 <- (v^2 * (B^2) / (4 * a))
   A <- exp(A1 - A2)
 
   # Bond Price
@@ -34,16 +34,16 @@ Vasicek1fPrice <- function(r, a, b, s, t){
 #' @param r
 #' @param a
 #' @param b
-#' @param s
+#' @param v
 #' @param t
 #'
 #' @return
 #' @export
 #'
 #' @examples
-VasicekYield <- function(r, a, b, s, t){
+VasicekYield <- function(r, a, b, v, t){
 
-  p <- VasicekPrice(r, a, b, s, t)
+  p <- VasicekPrice(r, a, b, v, t)
 
   -log(p)/t
 
@@ -54,26 +54,26 @@ VasicekYield <- function(r, a, b, s, t){
 #' @param r
 #' @param a
 #' @param b
-#' @param s
+#' @param v
 #' @param t
 #'
 #' @return
 #' @export
 #'
 #' @examples
-CIR1fPrice <- function(r, a, b, s, t){
+CIR1fPrice <- function(r, a, b, v, t){
   # returns the zero bond price that pays $1 at time t under a 1-factor CIR model
   # see Hull p.685
 
-  y <- sqrt(a^2 + 2 * s^2)
+  y <- sqrt(a^2 + 2 * v^2)
 
   # B term
-  temp1 <- (2 * (exp(y * t) - 1))
+  temp1 <- 2 * (exp(y * t) - 1)
   temp2 <- (y + a) * (exp(y * t) - 1) + 2 * y
   B <- temp1 / temp2
 
   temp3 <- 2 * y * exp(0.5 * (a + y) * t)
-  A <- (temp3 / temp2) ^ (2 * a * b * s ^-0.5)
+  A <- (temp3 / temp2) ^ (2 * a * b / v^2)
 
   # Bond Price
   P <- A * exp(-B * r)
@@ -86,16 +86,16 @@ CIR1fPrice <- function(r, a, b, s, t){
 #' @param r
 #' @param a
 #' @param b
-#' @param s
+#' @param v
 #' @param t
 #'
 #' @return
 #' @export
 #'
 #' @examples
-CIR1fYield <- function(r, a, b, s, t){
+CIR1fYield <- function(r, a, b, v, t){
 
-  p <- CIRPrice(r, a, b, s, t)
+  p <- CIRPrice(r, a, b, v, t)
 
   -log(p)/t
 
@@ -103,108 +103,72 @@ CIR1fYield <- function(r, a, b, s, t){
 
 #' Bond Price under 2-factor Vasicek Model
 #'
-#' @param r1
-#' @param r2
-#' @param a1
-#' @param a2
-#' @param s1
-#' @param s2
-#' @param u
-#' @param t
+#' @param r1 short rate at time t
+#' @param r2 mean reversion level for the short rate at time t
+#' @param a1 strength of mean reversion for short rate
+#' @param a2 strength of mean reversion for short rate mean reversion
+#' @param v1 annualized volatility of short rate
+#' @param v2 annualized volatility of short rate mean
+#' @param u mean reversion level for r2
+#' @param t maturity of zero-coupon bond
 #'
 #' @return
 #' @export
 #'
 #' @examples
-Vasicek2fPrice <- function(r1, r2, a1, a2, s1, s2, u, t){
-  # returns the zero bond price under a 2-factor Vasicek yield curve with a known
+Vasicek2fPrice <- function(r1, r2, a1, a2, v1, v2, u, t){
+  # returns the zero bond price under a 2-factor Vasicek yield curve
   # see hibbert, mowbray, and turnbull p.18
 
-  exp(.Vas2FA(a1, a2, s1, s2, u, t) - .Vas2FB1(a1, t) * r1 - .Vas2FB2(a1, a2, t) * r2)
+  .Vas2fA <- function(a1, a2, v1, v2, u, s){
+    # see hibbert, mowbray, and turnbull p.18
+    # computes the function A in the bond pricing equation
+    temp1 <- (.Vas2fB1(a1, s) - s) * (u - (v1 * v1) / (2 * a1 * a1))
+    temp2 <- .Vas2fB2(a1, a2, s) * u - (v1 * v1 * .Vas2fB1(a1, s) * .Vas2fB1(a1, s)) / (4 * a1)
+    temp3 <- (s / (a2 * a2) - 2 * (.Vas2fB2(a1, a2, s) + .Vas2fB1(a1, s)) / (a2 * a2))
+    temp4 <- 1 / ((a1 - a2) * (a1 - a2)) * (1 - exp(-2 * a1 * s)) / (2 * a1)
+    temp5 <- -2 * a1 / (a2 * (a1 - a2) * (a1 - a2)) * (1 - exp(-(a1 + a2) * s)) / (a1 + a2)
+    temp6 <- a1 * a1 / (a2 * a2 * (a1 - a2) * (a1 - a2)) * (1 - exp(-2 * a2 * s)) / (2 * a2)
+
+    ret <- temp1 + temp2 + (v2 * v2 / 2) * (temp3 + temp4 + temp5 + temp6)
+
+    return(ret)
+
+  }
+
+  .Vas2fB1 <- function(a, s) (1 - exp(-1 * a * s)) / a
+
+  .Vas2fB2 <- function(a1, a2, s){
+
+    temp1 <- (1 - exp(-1 * a2 * s)) / a2 - (1 - exp(-1 * a1 * s)) / a1
+
+    a1 / (a1 - a2) * temp1
+
+  }
+
+  exp(.Vas2FA(a1, a2, v1, v2, u, t) - .Vas2FB1(a1, t) * r1 - .Vas2FB2(a1, a2, t) * r2)
 
 }
 
 #' Bond Yield under 2-factor Vasicek Model
 #'
-#' @param r1
-#' @param r2
-#' @param a1
-#' @param a2
-#' @param s1
-#' @param s2
-#' @param u
-#' @param t
+#' @param r1 short rate at time t
+#' @param r2 mean reversion level for the short rate at time t
+#' @param a1 strength of mean reversion for short rate
+#' @param a2 strength of mean reversion for short rate mean reversion
+#' @param v1 annualized volatility of short rate
+#' @param v2 annualized volatility of short rate mean
+#' @param u mean reversion level for r2
+#' @param t maturity of zero-coupon bond
 #'
 #' @return
 #' @export
 #'
 #' @examples
-Vasicek2fYield <- function(r1, r2, a1, a2, s1, s2, u, t){
+Vasicek2fYield <- function(r1, r2, a1, a2, v1, v2, u, t){
 
-  p <- Vasicek2fPrice(r1, r2, a1, a2, s1, s2, u, t)
+  p <- Vasicek2fPrice(r1, r2, a1, a2, v1, v2, u, t)
 
   -log(p)/t
 
 }
-
-#' Calculate A parameter of 2 factor Vasicek Model
-#'
-#' @param a1
-#' @param a2
-#' @param s1
-#' @param s2
-#' @param u
-#' @param s
-#' @keywords internal
-.Vas2fA <- function(a1, a2, s1, s2, u, s){
-  # see hibbert, mowbray, and turnbull p.18
-  # computes the function A in the bond pricing equation
-  temp1 <- (.Vas2fB1(a1, s) - s) * (u - (s1 * s1) / (2 * a1 * a1))
-  temp2 <- .Vas2fB2(a1, a2, s) * u - (s1 * s1 * .Vas2fB1(a1, s) * .Vas2fB1(a1, s)) / (4 * a1)
-  temp3 <- (s / (a2 * a2) - 2 * (.Vas2fB2(a1, a2, s) + .Vas2fB1(a1, s)) / (a2 * a2))
-  temp4 <- 1 / ((a1 - a2) * (a1 - a2)) * (1 - exp(-2 * a1 * s)) / (2 * a1)
-  temp5 <- -2 * a1 / (a2 * (a1 - a2) * (a1 - a2)) * (1 - exp(-(a1 + a2) * s)) / (a1 + a2)
-  temp6 <- a1 * a1 / (a2 * a2 * (a1 - a2) * (a1 - a2)) * (1 - exp(-2 * a2 * s)) / (2 * a2)
-
-  ret <- temp1 + temp2 + (s2 * s2 / 2) * (temp3 + temp4 + temp5 + temp6)
-
-  return(ret)
-
-}
-
-#' Calculate B1 parameter of 2 factor Vasicek Model
-#'
-#' @param a1
-#' @param a2
-#' @param s1
-#' @param s2
-#' @param u
-#' @param s
-#' @keywords internal
-.Vas2fB1 <- function(a, s){
-  # see hibbert, mowbray, and turnbull p.18
-
-  (1 - exp(-1 * a * s)) / a
-
-}
-
-#' Calculate B2 parameter of 2 factor Vasicek Model
-#'
-#' @param a1
-#' @param a2
-#' @param s1
-#' @param s2
-#' @param u
-#' @param s
-#' @keywords internal
-.Vas2fB2 <- function(a1, a2, s){
-  # see hibbert, mowbray, and turnbull p.18
-
-  temp1 <- (1 - exp(-1 * a2 * s)) / a2 - (1 - exp(-1 * a1 * s)) / a1
-
-  a1 / (a1 - a2) * temp1
-
-
-}
-
-
